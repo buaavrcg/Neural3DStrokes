9e4a47ad7a55a9ce6c2c49fb70bebc035f7dbe3c
zhou <>
2023-10-15 13:32:39 +0800

    add three strokes

--- source/models.py
+++ source/models.py
@@ -44,7 +44,7 @@ class Model(nn.Module):
 
         # Construct MLPs. WARNING: Construction order may matter, if MLP weights are
         # being regularized.
-        self.nerf = NerfMLP()
+        self.nerf = StrokeField()
         if self.single_mlp:
             self.prop = self.nerf
         elif self.single_prop:
